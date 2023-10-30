e08d46c8bcc7a3e8f5f7bd4f9765d0a77329ec2a
Mahdi Pasandideh <mtipsd.git@gmail.com>
2023-10-30 21:35:37 +0330

    Added :: ts config

--- main.ts
+++ main.ts
@@ -1,1 +1,9 @@
 let username = "Dave";
+let numbersLength: number = 15;
+
+const lastName: string = "";
+const splittedName: Array<string> = lastName.split("/");
+
+const element = document.getElementById("myID");
+
+element?.addEventListener("click", (e) => {});
