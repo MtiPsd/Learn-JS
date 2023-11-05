d0c968bb2429a13945a31942e3dc9a120c19ad15
Mahdi Pasandideh <mtipsd.git@gmail.com>
2023-11-05 17:35:36 +0330

    Completed :: very basic of react + ts

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Counter from "./components/Counter";
 import Heading from "./components/Heading";
 import Section from "./components/Section";
+import List from "./components/List";
 
 function App() {
   const [counter, setCounter] = useState<number>(1);
@@ -11,6 +12,13 @@ function App() {
       <Heading title="Hello" />
       <Section>This is My Children</Section>
       <Counter setCounter={setCounter}>This is {counter}</Counter>
+
+      <List
+        items={["Coffee", "Words", "Tea"]}
+        render={(item: string) => (
+          <span className="gold">{item}</span>
+        )}
+      />
     </>
   );
 }
