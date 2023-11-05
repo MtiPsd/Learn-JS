f2abc4af945122593b5a499fff790120d09e1605
Mahdi Pasandideh <mtipsd.git@gmail.com>
2023-11-05 20:49:32 +0330

    Start :: learning useReducer with ts

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,29 +1,9 @@
-import { useCallback, useEffect, useState, useRef } from "react";
-
-interface User {
-  id: number;
-  username: string;
-}
+import Counter from "./Counter";
 
 function App() {
-  const [count, setCount] = useState<number>(0);
-  const [users, setUsers] = useState<Array<User> | null>(null);
-
-  const inputRef = useRef<HTMLInputElement>(null);
-
-  useEffect(() => {
-    return () => console.log("unmounting ...");
-  }, [users]);
-
-  const addTwo = useCallback((e: any) => {
-    setCount(prev => prev + 2);
-  }, []);
-
   return (
     <>
-      <h1>{count}</h1>
-      <button onClick={addTwo}>Add</button>
-      <input type="text" ref={inputRef} />
+      <Counter>{(num: number) => <>Current Count: {num}</>}</Counter>
     </>
   );
 }
