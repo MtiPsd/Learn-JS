ca4b79478e725ed9eb6e8cfb4829df8545ae7308
Mahdi Pasandideh <mtipsd.git@gmail.com>
2023-11-05 17:19:41 +0330

    Finished :: one exercise

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,11 +1,16 @@
+import { useState } from "react";
+import Counter from "./components/Counter";
 import Heading from "./components/Heading";
 import Section from "./components/Section";
 
 function App() {
+  const [counter, setCounter] = useState<number>(1);
+
   return (
     <>
       <Heading title="Hello" />
       <Section>This is My Children</Section>
+      <Counter setCounter={setCounter}>This is {counter}</Counter>
     </>
   );
 }
--- react-ts/src/components/Counter.tsx
+++ react-ts/src/components/Counter.tsx
@@ -1,5 +1,18 @@
-function Counter() {
-  return <div>Counter</div>;
+import { ReactNode } from "react";
+
+type CounterProps = {
+  setCounter: React.Dispatch<React.SetStateAction<number>>;
+  children: ReactNode;
+};
+
+function Counter({ setCounter, children }: CounterProps) {
+  return (
+    <>
+      <h1>{children}</h1>
+      <button onClick={() => setCounter(c => c + 1)}>+</button>
+      <button onClick={() => setCounter(c => c - 1)}>-</button>
+    </>
+  );
 }
 
 export default Counter;
